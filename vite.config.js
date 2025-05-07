f8dc9a9aef0d9680eaf217bedc679cd864744ea8
ozi <ozisadiqkasai@gmail.com>
2025-05-07 23:52:40 +0100

    router install

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensures assets load from root (critical for static hosting)
-  build: {
-    outDir: 'dist', // Output folder for production build
-    emptyOutDir: true, // Clears the 'dist' folder on each build
-  },
+
 });
